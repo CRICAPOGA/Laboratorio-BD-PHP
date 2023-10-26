701cb8ca2f290b3705cbee61dd6f93d472006614
CRICAPOGA <143299671+CRICAPOGA@users.noreply.github.com>
2023-10-26 18:52:08 -0500

    Add files via upload

---
+++ InsertarDatos.php
@@ -0,0 +1,32 @@
+﻿ <html>
+ <head>
+ <title>InsertarDatos.php</title>
+ <meta http-equiv='content-type' content='text/html; charset=UTF-8'/>
+ </head>
+ <body>
+ <BODY BACKGROUND="imagenDeFondo.jpg">
+
+ <form action='InsertarDatos.php' method='post'>
+	<div>Cédula  : <input type='text' name='cedula' value=" "/> </div>
+	<div>Nombre :   <input type='text' name='nombre' value=" "/> </div>
+        <div>Edad :   <input type='text' name='edad' value=" "/> </div>
+	<div><input type='submit' name='insert' value='Insertar registro'/></div>
+ </form>
+
+ <?php 
+	$link = new mysqli("localhost", "root", "", "programaciondos");
+	
+	$sql = "INSERT INTO tablita (cedula, nombre, edad) VALUES ('$_POST[cedula]', '$_POST[nombre]', '$_POST[edad]')";
+
+
+ if ($link->query($sql) === TRUE) {
+    echo "New record created successfully";
+ } else {
+    echo "Error: " . $sql . "<br>" . $link->error;
+ }
+
+
+ ?>
+
+ </body>
+ </html>
